056f80442f2598ae577103b712355618016ad69d
fstahnke <github@fabian-stahnke.de>
2015-09-25 16:38:48 +0200

    Add analysis of scaling

---
+++ src/org/deidentifier/arx/benchmark/BenchmarkAnalysisRecordScaling.java
@@ -0,0 +1,210 @@
+/*
+ * Source code of the experiments for the entropy metric
+ * 
+ * Copyright (C) 2015 Fabian Prasser
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.deidentifier.arx.benchmark;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.deidentifier.arx.benchmark.BenchmarkSetup.BenchmarkAlgorithm;
+import org.deidentifier.arx.benchmark.BenchmarkSetup.BenchmarkDataset;
+import org.deidentifier.arx.benchmark.BenchmarkSetup.BenchmarkPrivacyModel;
+import org.deidentifier.arx.benchmark.BenchmarkSetup.BenchmarkUtilityMeasure;
+
+import de.linearbits.objectselector.Selector;
+import de.linearbits.subframe.analyzer.Analyzer;
+import de.linearbits.subframe.graph.Field;
+import de.linearbits.subframe.graph.Labels;
+import de.linearbits.subframe.graph.Plot;
+import de.linearbits.subframe.graph.PlotLinesClustered;
+import de.linearbits.subframe.graph.Point2D;
+import de.linearbits.subframe.graph.Point3D;
+import de.linearbits.subframe.graph.Series2D;
+import de.linearbits.subframe.graph.Series3D;
+import de.linearbits.subframe.io.CSVFile;
+import de.linearbits.subframe.render.GnuPlotParams;
+import de.linearbits.subframe.render.GnuPlotParams.KeyPos;
+import de.linearbits.subframe.render.LaTeX;
+import de.linearbits.subframe.render.PlotGroup;
+
+/**
+ * BenchmarkAnalysis analyzing scaling of the different algorithms. x-Axis:
+ * Scaling factor (records, QIs, K-value) y-Axis: Time Plot-Type: Line Plot
+ * 
+ * @author Fabian Prasser
+ */
+public class BenchmarkAnalysisRecordScaling {
+
+    /**
+     * Choose benchmarkConfig to run and comment others out.
+     */
+     private static final String benchmarkConfig =
+     "benchmarkConfig/recordScaling.xml";
+//    private static final String benchmarkConfig = "benchmarkConfig/QIScaling.xml";
+    // private static final String benchmarkConfig =
+    // "benchmarkConfig/kScaling.xml";
+
+    /**
+     * Main
+     * 
+     * @param args
+     * @throws IOException
+     * @throws ParseException
+     */
+    public static void main(String[] args) throws IOException, ParseException {
+
+        List<PlotGroup> groups = new ArrayList<PlotGroup>();
+        BenchmarkSetup setup = new BenchmarkSetup(benchmarkConfig);
+        CSVFile file = new CSVFile(new File(setup.getOutputFile()));
+        
+        groups.add(analyze(file, null, null, null, null, 0d));
+        LaTeX.plot(groups, setup.getPlotFile());
+
+    }
+
+    /**
+     * Performs the analysis
+     * 
+     * @param file
+     * @param suppression
+     * @param algorithm
+     * @param model
+     * @param measure
+     * @param data
+     * @return
+     * @throws ParseException
+     */
+    private static PlotGroup analyze(CSVFile file,
+                                     BenchmarkDataset data,
+                                     BenchmarkUtilityMeasure measure,
+                                     BenchmarkPrivacyModel model,
+                                     BenchmarkAlgorithm algorithm,
+                                     double suppression) throws ParseException {
+
+        // Selects according rows
+        Selector<String[]> selectorRGR = file.getSelectorBuilder()
+                                             .field("Dataset")
+                                             .equals(BenchmarkDataset.ADULT.toString())
+                                             .and()
+                                             .field("UtilityMeasure")
+                                             .equals(BenchmarkUtilityMeasure.LOSS.toString())
+                                             .and()
+                                             .field("PrivacyModel")
+                                             .equals(BenchmarkPrivacyModel.K5_ANONYMITY.toString())
+                                             .and()
+                                             .field("Algorithm")
+                                             .equals(BenchmarkAlgorithm.RECURSIVE_GLOBAL_RECODING.toString())
+                                             .and()
+                                             .field("Suppression")
+                                             .equals("0.1")
+                                             .build();
+
+        // Selects according rows
+        Selector<String[]> selectorFlash = file.getSelectorBuilder()
+                                               .field("Dataset")
+                                               .equals(BenchmarkDataset.ADULT.toString())
+                                               .and()
+                                               .field("UtilityMeasure")
+                                               .equals(BenchmarkUtilityMeasure.LOSS.toString())
+                                               .and()
+                                               .field("PrivacyModel")
+                                               .equals(BenchmarkPrivacyModel.K5_ANONYMITY.toString())
+                                               .and()
+                                               .field("Algorithm")
+                                               .equals(BenchmarkAlgorithm.FLASH.toString())
+                                               .and()
+                                               .field("Suppression")
+                                               .equals("0.1")
+                                               .build();
+
+        // Selects according rows
+        Selector<String[]> selectorTassa = file.getSelectorBuilder()
+                                               .field("Dataset")
+                                               .equals(BenchmarkDataset.ADULT.toString())
+                                               .and()
+                                               .field("UtilityMeasure")
+                                               .equals(BenchmarkUtilityMeasure.LOSS.toString())
+                                               .and()
+                                               .field("PrivacyModel")
+                                               .equals(BenchmarkPrivacyModel.K5_ANONYMITY.toString())
+                                               .and()
+                                               .field("Algorithm")
+                                               .equals(BenchmarkAlgorithm.TASSA.toString())
+                                               .and()
+                                               .field("Suppression")
+                                               .equals("0.0")
+                                               .build();
+
+        // Read data into 2D series
+        Series2D rgrSeries = new Series2D(file,
+                                          selectorRGR,
+                                          new Field("Records", Analyzer.VALUE),
+                                          new Field("Runtime", Analyzer.VALUE));
+
+        // Read data into 2D series
+        Series2D flashSeries = new Series2D(file,
+                                            selectorFlash,
+                                            new Field("Records", Analyzer.VALUE),
+                                            new Field("Runtime", Analyzer.VALUE));
+
+        // Read data into 2D series
+        Series2D tassaSeries = new Series2D(file,
+                                            selectorTassa,
+                                            new Field("Records", Analyzer.VALUE),
+                                            new Field("Runtime", Analyzer.VALUE));
+
+        // Dirty hack for creating a 3D series from two 2D series'
+        Series3D series = new Series3D(file,
+                                       selectorRGR,
+                                       new Field("Dataset"), // Cluster
+                                       new Field("UtilityMeasure"), // Type
+                                       new Field("PrivacyModel")); // Value
+        series.getData().clear();
+        for (Point2D point : rgrSeries.getData()) {
+            series.getData().add(new Point3D(point.x, "Runime (RGR)", point.y));
+        }
+        for (Point2D point : flashSeries.getData()) {
+            series.getData().add(new Point3D(point.x, "Runime (Flash)", point.y));
+        }
+        int tassaScalingFactor = 10;
+        for (Point2D point : tassaSeries.getData()) {
+            series.getData().add(new Point3D(point.x, "Runime (Tassa) [divided by " + tassaScalingFactor + "]", String.valueOf((Double.valueOf(point.y) / tassaScalingFactor))));
+        }
+
+        // Plot
+        List<Plot<?>> plots = new ArrayList<Plot<?>>();
+        plots.add(new PlotLinesClustered(BenchmarkDataset.ADULT.toString() + " / " +
+                                         BenchmarkUtilityMeasure.LOSS.toString() + " / " +
+                                         BenchmarkPrivacyModel.K5_ANONYMITY.toString() + " / 0.1",
+                                         new Labels("Records", "Time [ms]"),
+                                         series));
+
+        GnuPlotParams params = new GnuPlotParams();
+        params.colorize = true;
+        params.rotateXTicks = 0;
+        params.keypos = KeyPos.TOP_LEFT;
+        params.size = 1.0d;
+        params.ratio = 0.5d;
+        return new PlotGroup("Scaling of anonymization algorithms with quasi-identifiers. ",
+                             plots,
+                             params,
+                             1.0d);
+    }
+}
